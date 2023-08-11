01cfd8afb1dd94b0f9fd3c531dc38f9d199d48fa
Yutika Nemade <109644250+Yutika-Nemade@users.noreply.github.com>
2023-08-11 11:18:26 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -3,10 +3,14 @@ import mongoose from "mongoose"
 const connectDB = async () => {
     try{
         const conn = await mongoose.connect(process.env.MONGO_URL);
-        // console.log(`Connected to mongoDB database ${conn.connection.host}`);
     }catch(error){
-        // console.log(`Error in mongoDB ${error}`)
     }
 } 
+
+connectDB().then(() => {
+    app.listen(PORT, () => {
+        console.log("listening for requests");
+    })
+})
 
 export default connectDB;
